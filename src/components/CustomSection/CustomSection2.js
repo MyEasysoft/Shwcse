d5b12f579763dcac563eb262f622b2e9bd7a5097
admis <admisrael2012@gmail.com>
2024-02-21 15:59:16 +0100

    Almost final

--- src/components/CustomSection/CustomSection2.js
+++ src/components/CustomSection/CustomSection2.js
@@ -11,6 +11,7 @@ import slidder from '../../assets/slidder.PNG';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faMessage, faHeart, faSignIn, faEnvelope} from '@fortawesome/free-solid-svg-icons'
 import classNames from 'classnames';
+import right_arrow from '../../assets/right_arrow_trans.png';
 
 
 const CustomSectionComponent2 = props =>{
@@ -34,18 +35,36 @@ const CustomSectionComponent2 = props =>{
         </div>
 
         <div>
-          <div className={css.col45}><img className={css.resize_image} src={img3}/></div>
-          <div className={css.col20}><img className={css.resize_image} src={img4}/></div>
-          <div className={css.col35}><img className={css.resize_image} src={img5}/></div>
+          <div className={css.col45}>
+            <img className={classNames(css.loca_bg,css.resize_image)} src={img3}/>
+            <img className={classNames(css.carousel__btn,css.carousel__btn__prev)} src={right_arrow}/>
+          </div>
+          <div className={css.col20}>
+            <img className={classNames(css.loca_bg,css.resize_image)} src={img4}/>
+            <img className={classNames(css.carousel__btn,css.carousel__btn__prev)} src={right_arrow}/>
+          </div>
+          <div className={css.col35}>
+            <img className={classNames(css.loca_bg,css.resize_image)} src={img5}/>
+            <img className={classNames(css.carousel__btn,css.carousel__btn__prev)} src={right_arrow}/>
+          </div>
         </div>
 
       </div>
 
       <div className={classNames(css.container, css.desktop)}>
         <div>
-          <div className={css.col20}><img className={css.resize_image} src={img6}/></div>
-          <div className={css.col35}><img className={css.resize_image} src={img7}/></div>
-          <div className={css.col45}><img className={css.resize_image} src={img8}/></div>
+          <div className={css.col20}>
+            <img className={classNames(css.loca_bg,css.resize_image)} src={img6}/>
+            <img className={classNames(css.carousel__btn,css.carousel__btn__prev)} src={right_arrow}/>
+          </div>
+          <div className={css.col35}>
+            <img className={classNames(css.loca_bg,css.resize_image)} src={img7}/>
+            <img className={classNames(css.carousel__btn,css.carousel__btn__prev)} src={right_arrow}/>
+          </div>
+          <div className={css.col45}>
+            <img className={classNames(css.loca_bg,css.resize_image)} src={img8}/>
+            <img className={classNames(css.carousel__btn,css.carousel__btn__prev)} src={right_arrow}/>
+          </div>
         </div>
 
       </div>
@@ -55,12 +74,33 @@ const CustomSectionComponent2 = props =>{
                 Locations?
             </h1>
             <img className={css.h_20}  src={slidder}/>
-            <img className={css.resize_image} src={img3}/>
-            <img className={css.resize_image} src={img4}/>
-            <img className={css.resize_image} src={img5}/>
-           <img className={css.resize_image} src={img4}/>
-            <img className={css.resize_image} src={img5}/>
-            <img className={css.resize_image} src={img3}/>
+          <div className={classNames(css.img_con)}>
+            <img className={classNames(css.loca_bg,css.resize_image)} src={img3}/>
+            <img className={classNames(css.carousel__btn,css.carousel__btn__prev)} src={right_arrow}/>
+          </div>
+          <div className={classNames(css.img_con)}>
+            <img className={classNames(css.loca_bg,css.resize_image)} src={img4}/>
+            <img className={classNames(css.carousel__btn,css.carousel__btn__prev)} src={right_arrow}/>
+          </div>
+          <div className={classNames(css.img_con)}>
+            <img className={classNames(css.loca_bg,css.resize_image)} src={img5}/>
+            <img className={classNames(css.carousel__btn,css.carousel__btn__prev)} src={right_arrow}/>
+          </div>
+
+          <div className={classNames(css.img_con)}>
+            <img className={classNames(css.loca_bg,css.resize_image)} src={img6}/>
+            <img className={classNames(css.carousel__btn,css.carousel__btn__prev)} src={right_arrow}/>
+          </div>
+          <div className={classNames(css.img_con)}>
+            <img className={classNames(css.loca_bg,css.resize_image)} src={img7}/>
+            <img className={classNames(css.carousel__btn,css.carousel__btn__prev)} src={right_arrow}/>
+          </div>
+          <div className={classNames(css.img_con)}>
+            <img className={classNames(css.loca_bg,css.resize_image)} src={img8}/>
+            <img className={classNames(css.carousel__btn,css.carousel__btn__prev)} src={right_arrow}/>
+          </div>
+
+
       </div>
 
 
