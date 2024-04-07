b570e2b2189ba17e0228ec8e6c1581882ce5f48e
admis <admisrael2012@gmail.com>
2024-04-07 03:10:01 +0100

    Shwcse Landing Page

--- src/components/CustomSection/CustomSection3b.js
+++ src/components/CustomSection/CustomSection3b.js
@@ -33,38 +33,68 @@ const CustomSectionComponent3b = props =>{
             <div className={classNames(css.h_abs_7, css.col_3)}>
                 <div className={classNames(css.h_6, css.col_12)}>
                     <img src={img14} className={css.resize}/>
+                    <div className={css.overlay}>
+                        <div><h2>Sneakers</h2></div>
+                        <div><img className={classNames(css.carousel__btn)} src={right_arrow}/></div>
+                    </div>
                 </div>
                 <div className={classNames(css.h_4,css.subHeight_3, css.col_12)}>
                     <img src={img9} className={css.resize}/>
+                    <div className={css.overlay}>
+                        <div><h2>Tops</h2></div>
+                        <div><img className={classNames(css.carousel__btn)} src={right_arrow}/></div>
+                    </div>
                 </div>
             </div>
             <div className={classNames(css.h_abs_7, css.col_3)}>
                 <div className={classNames(css.h_7, css.col_12)}>
                     <img src={img10} className={css.resize}/>
+                    <div className={css.overlay}>
+                        <div><h2>Pants</h2></div>
+                        <div><img className={classNames(css.carousel__btn)} src={right_arrow}/></div>
+                    </div>
                 </div>
                 <div className={classNames(css.h_3,css.subHeight_1, css.col_12)}>
                     <img src={img15} className={css.resize}/>
+                    <div className={css.overlay}>
+                        <div><h3>Purse and Bags</h3></div>
+                        <div><img className={classNames(css.carousel__btn)} src={right_arrow}/></div>
+                    </div>
                 </div>
             </div>
             <div className={classNames(css.h_abs_7, css.col_3)}>
                 <div className={classNames(css.h_6, css.col_12)}>
                     <img src={img17} className={css.resize}/>
+                    <div className={css.overlay}>
+                        <div><h2>Sweaters</h2></div>
+                        <div><img className={classNames(css.carousel__btn)} src={right_arrow}/></div>
+                    </div>
                 </div>
                 <div className={classNames(css.h_4,css.subHeight_2, css.col_12)}>
                     <img src={img13} className={css.resize}/>
+                    <div className={css.overlay}>
+                        <div><h3>Coats and Jackets</h3></div>
+                        <div><img className={classNames(css.carousel__btn)} src={right_arrow}/></div>
+                    </div>
                 </div>
             </div>
             <div className={classNames(css.h_abs_7, css.col_3)}>
                 <div className={classNames(css.h_5, css.col_12)}>
                     <img src={img16} className={css.resize}/>
+                    <div className={css.overlay}>
+                        <div><h2>T-Shirts</h2></div>
+                        <div><img className={classNames(css.carousel__btn)} src={right_arrow}/></div>
+                    </div>
                 </div>
                 <div className={classNames(css.h_5, css.col_12)}>
                     <img src={img11} className={css.resize}/>
+                    <div className={css.overlay}>
+                        <div><h2>Skirts</h2></div>
+                        <div><img className={classNames(css.carousel__btn)} src={right_arrow}/></div>
+                    </div>
                 </div>
             </div>
            
-            
-
       </div>
 
       <div className={css.mobile}>
